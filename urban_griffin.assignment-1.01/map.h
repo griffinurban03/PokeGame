ce5f12222b4c0cef177abd155411a413daeab57a
Griffin <griffin@urbsfamily.com>
2026-02-04 14:26:57 -0600

    Renamed and added functions to improve code seperation. Added additional variables to store gate locations.

--- urban_griffin.assignment-1.01/map.h
+++ urban_griffin.assignment-1.01/map.h
@@ -2,9 +2,17 @@ typedef struct map {
 	int width;
 	int height;
 	char **cells;
+	int north_gate[2];
+	int south_gate[2];
+	int east_gate[2];
+	int west_gate[2];
 } map;
 
 int map_init(map *m, int w, int h);
 int map_destroy(map *m);
-int map_generate(map *m);
 int map_print(map *m);
+int map_generate(map *m);
+int map_generate_terrain(map *m);
+int map_generate_borders(map *m);
+int map_generate_paths(map *m);
+int map_generate_pokeshops(map *m);
